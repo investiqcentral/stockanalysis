803acba3043f4e40224a36a33ad376cc9c3396b5
investiqcentral <investiqcentral@gmail.com>
2025-10-10 11:03:24 +0700

    Update macro_analysis.py

--- macro_analysis.py
+++ macro_analysis.py
@@ -14,7 +14,7 @@ import re
 
 st.set_page_config(layout="wide", page_title="Economic Analysis Dashboard", page_icon="./Image/logo.png")
 
-ai_model = 'meta-llama/llama-prompt-guard-2-86m'
+ai_model = 'llama-3.3-70b-versatile'
 FRED_API_KEY = st.secrets["FRED_API_KEY"]
 NUM_POINTS = 20
 GROWTH_COLOR = 'red'
